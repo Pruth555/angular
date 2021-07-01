4a54cd27d291911834ac6c52562ce93e9e55ca4a
Pruth555 <86759571+Pruth555@users.noreply.github.com>
2021-07-01 11:52:23 +0530

    Add files via upload

---
+++ bookingconfirm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bookingconfirm',
+  templateUrl: './bookingconfirm.component.html',
+  styleUrls: ['./bookingconfirm.component.css']
+})
+export class BookingconfirmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
